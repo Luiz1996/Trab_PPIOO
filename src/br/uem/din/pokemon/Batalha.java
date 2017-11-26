d5e619e50d5f520e1099e40d4467c6f7d48b01e0
luiz_flavio_p@hotmail.com <>
2017-11-26 12:07:01 -0200

    Trab v2

--- src/br/uem/din/pokemon/Batalha.java
+++ src/br/uem/din/pokemon/Batalha.java
@@ -46,7 +46,7 @@ public class Batalha {
                     inicializa = false;
                     Pokemon.todosPokemons();
                     
-                    
+                    //
 
                     break;
 
