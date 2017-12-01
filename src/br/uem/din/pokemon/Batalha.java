3244fd66b6046616cd1dc44b948357b49f094d00
luiz.pereira <luiz.pereira@DELL_FZ_3KGL5LV.FZ.LOCAL>
2017-12-01 17:14:12 -0200

    Pequenas mudanças

--- src/br/uem/din/pokemon/Batalha.java
+++ src/br/uem/din/pokemon/Batalha.java
@@ -6,20 +6,18 @@ import java.util.Scanner;
 
 public class Batalha {
     Pokemon Pokemon = new Pokemon();
-    
-    Pokemon[] meuPokemon = new Pokemon [6];
+    Pokemon[] meusPokemons = new Pokemon[6];
     int controle[] = new int [6];
     
-    
-       
     public void carregarTabelas(){
        Scanner input = new Scanner(System.in);
         System.out.println("Escolha seus Pokemons:");
+        
         for (int i = 0; i < 6; i++){
             System.out.print("Escolha seu " + (i+1) + "º Pokemon:" );
             int escolha = input.nextInt();        
-            meuPokemon[i] = Pokemon.escolhaPokemon(escolha-1);
-            System.out.println("Pokemon escolhido: " + meuPokemon[i].nome);
+            meusPokemons[i] = Pokemon.todosPokemons(escolha-1);
+            System.out.println("Pokemon escolhido: " + meusPokemons[i].nome);
             controle[i] = escolha; //Para controlar se o Pokemon já foi escolhido
         }
     }
@@ -36,26 +34,32 @@ public class Batalha {
             System.out.println("Ecolha o modo de jogo:");
             System.out.println("[1] Humano vs Humano.");
             System.out.println("[2] Humano vs Máquina.");
+            System.out.println("[3] Máquina vs Máquina.");
             opcao = input.nextInt();
             limparTela();
-            if(opcao < 1 || opcao > 2){
-                System.out.println("OPCAO INVALIDA!!!");
+            if(opcao < 1 || opcao > 3){
+                System.out.println("Opção inválida!");
+                System.out.println("Digite um valor entre 1 e 3.");
                 continue inicializa;
             }
         
             switch (opcao){
                 case 1: 
                     inicializa = false;
-                    Pokemon.mostraPokemons();
-                    
-                    //
-
+                    Pokemon.todosPokemons();
+                    Jogo.limparTela();
+                   
+                    //será realizado 2x porque são duas pessoas.
+                    carregarTabelas();
+                    carregarTabelas();
                     break;
 
                 case 2:
-                    System.out.println("Olá!");
-
-
+                    System.out.println("Humano vs Máquina");
+                    break;
+                    
+                case 3:
+                    System.out.println("Máquina vs Máquina");
                     break;
             }
         } 
