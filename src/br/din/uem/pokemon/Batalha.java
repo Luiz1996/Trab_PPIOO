b5647cd578c8087480cdbd1f1b66c5ad05996e26
TransDesk <TransDesk@comp2>
2017-12-18 16:58:41 -0200

    Tá fumegando!

--- src/br/din/uem/pokemon/Batalha.java
+++ src/br/din/uem/pokemon/Batalha.java
@@ -1,15 +1,13 @@
 package br.din.uem.pokemon;
 
-import br.din.uem.fronteira.Inicio;
 import java.io.IOException;
 import java.util.Scanner;
+import static jdk.nashorn.tools.ShellFunctions.input;
 
 public class Batalha {
 
-    
-   Pokemon Pokemon = new Pokemon();
+    static Pokemon Pokemon = new Pokemon();
 
-    
     public void inicializarJogadores() throws IOException {
         Scanner input = new Scanner(System.in);
         int opcao;
@@ -24,7 +22,7 @@ public class Batalha {
             System.out.println("[3] Máquina vs Máquina.");
             System.out.print("Opção:");
             opcao = input.nextInt();
-            
+
             if (opcao < 1 || opcao > 3) {
                 System.out.println("Opção inválida!");
                 System.out.println("Digite um valor entre 1 e 3.");
@@ -33,23 +31,104 @@ public class Batalha {
 
             switch (opcao) {
                 case 1:
-                    
+
                     inicializa = false;
-                    Humano.carregarTabelas1();
-                    Humano.carregarTabelas2();
+                    carregarTabelas1();
+                    carregarTabelas2();
                     System.out.println("-------------------------------------------------------------- TIMES FORMADOS COM SUCESSO --------------------------------------------------------------");
+                    Humano.escolherComando(1);
+                    Humano.escolherComando(2);
                     break;
 
                 case 2:
                     System.out.println("Opção não implementada.");
                     continue inicializa;
-                    
+
                 case 3:
                     System.out.println("Opção não implementada.");
                     continue inicializa;
-                    
+
             }
         }
+    }
+
+    static Pokemon[] jogador1 = new Pokemon[6];
+
+    public static void carregarTabelas1() {
+        Scanner input = new Scanner(System.in);
+        Especie especieEscolhida;
+        Ataque ataques1[] = new Ataque[4];
+        int k;
+
+        System.out.println("\n"); //Pular 1 linha.
+        System.out.println("Jogador 1, sua vez! Escolha seus Pokemóns...");
+        for (int i = 0; i < 6; i++) {
+            Especie.todosPokemons();
+            System.out.print("Escolha seu " + (i + 1) + "º Pokemon:");
+            int escolha = input.nextInt();
+            especieEscolhida = Especie.escolhaPokemon(escolha - 1);
+            Ataque.todosAtaques();
+            for (k = 0; k < 4; k++) {
+                System.out.print("Escolha seu " + (k + 1) + "º Ataque:");
+                int escolhaatk = input.nextInt();
+                ataques1[k] = Ataque.escolhaAtaque(escolhaatk - 1);
+            } 
+            jogador1[i] = Pokemon.escolhaPokemon(especieEscolhida, ataques1[0], ataques1[1], ataques1[2], ataques1[3]);
+            //System.out.println("Pokemon escolhido: " + jogador1[i].especie);
+        }
+    }
+
+    static Pokemon[] jogador2 = new Pokemon[6];
+
+    public static void carregarTabelas2() {
+        Scanner input = new Scanner(System.in);
+        Especie especieEscolhida;
+        Ataque ataques2[] = new Ataque[4];
+        int k;
+
+        System.out.println("\n"); //Pular 1 linha.
+        System.out.println("Jogador 2, sua vez! Escolha seus Pokemóns...");
+        for (int i = 0; i < 6; i++) {
+            Especie.todosPokemons();
+            System.out.print("Escolha seu " + (i + 1) + "º Pokemon:");
+            int escolha = input.nextInt();
+            especieEscolhida = Especie.escolhaPokemon(escolha - 1);
+            Ataque.todosAtaques();
+            for (k = 0; k < 4; k++) {
+                System.out.print("Escolha seu " + (k + 1) + "º Ataque:");
+                int escolhaatk = input.nextInt();
+                ataques2[k] = Ataque.escolhaAtaque(escolhaatk - 1);
+            }
+            jogador2[i] = Pokemon.escolhaPokemon(especieEscolhida, ataques2[0], ataques2[1], ataques2[2], ataques2[3]);
+            //System.out.println("Pokemon escolhido: " + jogador1[i].especie);         
+        }
+    }
+
+    public static Pokemon[] getJogador1() {
+        return jogador1;
+    }
+
+    public static void setJogador1(Pokemon[] jogador1) {
+        Batalha.jogador1 = jogador1;
+    }
+
+    public static Pokemon[] getJogador2() {
+        return jogador2;
+    }
+
+    public static void setJogador2(Pokemon[] jogador2) {
+        Batalha.jogador2 = jogador2;
+    }
+
+    public static void trocaPokemon(Pokemon jogador[]) { //Método para trocar o Pokemon
+        int i;
+        Pokemon auxtroca[] = new Pokemon[1];
+        auxtroca[0] = jogador[0];
+        for (i = 0; i < 5; i++) {
+            jogador[i] = jogador[i + 1];
+        }
+        jogador[5] = auxtroca[0];
+
     }
 }
 
--- src/br/din/uem/pokemon/Humano.java
+++ src/br/din/uem/pokemon/Humano.java
@@ -4,8 +4,11 @@ import static br.din.uem.pokemon.Pokemon.escolhaPokemon;
 import java.util.Scanner;
 
 public class Humano {
+    
+    static Pokemon vetor[];
+    
 
-    public static void escolherComando() {
+    public static void escolherComando(int jogador) {
         Scanner input = new Scanner(System.in);
         int opcao;
         boolean escolha = true;
@@ -23,7 +26,17 @@ public class Humano {
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Ops!!1");
+                    if (jogador == 1){
+                       vetor = Batalha.getJogador1();
+                       Batalha.trocaPokemon(vetor);
+                       jogador = 2;
+                       Batalha.setJogador1(vetor);
+                    } else if (jogador == 2) {
+                       vetor = Batalha.getJogador2();
+                       Batalha.trocaPokemon(vetor);
+                       jogador = 1;
+                       Batalha.setJogador2(vetor);
+                    }
                     break;
 
                 case 2:
@@ -34,55 +47,5 @@ public class Humano {
         }
     }
 
-    private static Pokemon Pokemon = new Pokemon();
-
-    public static void carregarTabelas1() {
-        Scanner input = new Scanner(System.in);
-        Pokemon[] jogador1 = new Pokemon[6];
-        Especie especieEscolhida;
-        Ataque ataques1[] = new Ataque[4];
-        int k;
-
-        Especie.todosPokemons();
-        System.out.println("\n"); //Pular 1 linha.
-        System.out.println("Jogador 1, sua vez! Escolha seus Pokemóns...");
-        for (int i = 0; i < 6; i++) {
-            System.out.print("Escolha seu " + (i + 1) + "º Pokemon:");
-            int escolha = input.nextInt();
-            especieEscolhida = Especie.escolhaPokemon(escolha - 1);
-            Ataque.todosAtaques();
-            for (k = 0; k < 4; k++) {
-                System.out.print("Escolha seu " + (k + 1) + "º Ataque:");
-                int escolhaatk = input.nextInt();
-                ataques1[k] = Ataque.escolhaAtaque(escolhaatk - 1);
-            }
-            jogador1[i] = Pokemon.escolhaPokemon(especieEscolhida, ataques1[0], ataques1[1], ataques1[2], ataques1[3]);
-            //System.out.println("Pokemon escolhido: " + jogador1[i].especie);
-        }
-    }
-
-    public static void carregarTabelas2() {
-        Scanner input = new Scanner(System.in);
-        Pokemon[] jogador2 = new Pokemon[6];
-        Especie especieEscolhida;
-        Ataque ataques2[] = new Ataque[4];
-        int k;
-
-        Especie.todosPokemons();
-        System.out.println("\n"); //Pular 1 linha.
-        System.out.println("Jogador 1, sua vez! Escolha seus Pokemóns...");
-        for (int i = 0; i < 6; i++) {
-            System.out.print("Escolha seu " + (i + 1) + "º Pokemon:");
-            int escolha = input.nextInt();
-            especieEscolhida = Especie.escolhaPokemon(escolha - 1);
-            Ataque.todosAtaques();
-            for (k = 0; k < 4; k++) {
-                System.out.print("Escolha seu " + (k + 1) + "º Ataque:");
-                int escolhaatk = input.nextInt();
-                ataques2[k] = Ataque.escolhaAtaque(escolhaatk - 1);
-            }
-            jogador2[i] = Pokemon.escolhaPokemon(especieEscolhida, ataques2[0], ataques2[1], ataques2[2], ataques2[3]);
-            //System.out.println("Pokemon escolhido: " + jogador1[i].especie);
-        }
-    }
+   
 }
